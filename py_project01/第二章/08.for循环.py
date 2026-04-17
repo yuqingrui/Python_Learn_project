9e24460568dd80bfa67806b43fd7868893345252
yuqqingrui <540148550@qq.com>
2026-04-17 14:39:51 +0800

    6666

--- py_project01/第二章/08.for循环.py
+++ py_project01/第二章/08.for循环.py
@@ -70,15 +70,120 @@ print("*", end="")：end表示的是每一次输出以什么结束；默认\n，
 print("------------------------")
 
 # 需求1：根据输入的直角边的边长，打印等腰直角三角形
-# n = int(input("请输入直角边的边长："))
-#
-# for i in range(1, n + 1):  # 外层循环控制行数
-#     for j in range(i):  # 内层循环控制每行打印的星号数量
-#         print("*", end=" ")
-#     print()
+n = int(input("请输入直角边的边长："))
 
-
-for i in range(1, 7):  # 外层循环控制行数
+for i in range(1, n + 1):  # 外层循环控制行数
     for j in range(i):  # 内层循环控制每行打印的星号数量
         print("*", end=" ")
     print()
+
+
+print("------------------------")
+
+# 需求2：根据输入的数字，打印对应的数字金字塔
+n = int(input("请输入数字金字塔的层数："))
+
+for i in range(1, n + 1):
+    for j in range(1, i + 1):
+        print(j, end=" ")
+    print()
+
+print("------------------------")
+
+# 需求3：打印国际象棋棋盘
+for i in range(8):  # 8行
+    for j in range(8):  # 8列
+        if (i + j) % 2 == 0:
+            print("■", end=" ")
+        else:
+            print("□", end=" ")
+    print()
+
+"""
+案例1：根据输入的用户名密码执行登录操作，具体要求如下：
+1. 正确的用户名和密码为admin/666888、zhangsan/123456、taoge/888666
+2. 输入用户名和密码进行登录，直到登录成功，程序结束运行；如果登录失败，则继续输入用户名和密码进行登录
+3. 输入的用户名和密码不能为空！
+4. 登录成功：输出 "登录成功，进入B站首页~"
+5. 登录失败：输出 "用户名或密码错误，请重新输入！"
+
+关键字：
+    break：只能够出现在循环中，表示结束、跳出循环的含义（break跳出循环时，while后面的else中的代码将不会执行）
+    continue：只能够出现在循环中，表示中断本次循环，直接进入下一次循环
+"""
+
+while True:
+    # 1. 接收输入的用户名和密码
+    username = input("请输入正确的用户名：")
+    password = input("请输入正确的密码：")
+
+    # 2. 校验：输入的用户名和密码不能为空！
+    if username == "" or password == "":
+        print("输入的用户名和密码不能为空！请重新输入")
+        continue  # 结束当前循环，直接进入下一轮循环
+
+    # 3. 判断用户名和密码的正确性，执行登录操作
+    if username == "admin" and password == "666888":
+        print("登录成功，进入B站首页~")
+        break  # 跳出循环
+    elif username == "zhangsan" and password == "123456":
+        print("登录成功，进入B站首页~")
+        break  # 跳出循环
+    elif username == "taoge" and password == "888666":
+        print("登录成功，进入B站首页~")
+        break  # 跳出循环
+    else:
+        print("用户名或密码错误，请重新输入！")
+
+print("------------------------")
+
+"""
+案例2：猜数字游戏
+1. 系统随机生成一个随机数
+2. 用户根据提示猜数字，并将所猜的数字输入系统
+3. 如果猜错，系统给出提示是猜大了，还是猜小了，然后继续输入猜的数字
+4. 如果猜对，系统自动退出，游戏结束
+"""
+
+import random
+random_num = random.randint(1, 100)  # 生成随机数
+
+while True:
+    # 接收输入的数字
+    num = int(input("请输入一个数字："))
+
+    # 比较
+    if num > random_num:
+        print("您输入的数字太大了！")
+    elif num < random_num:
+        print("您输入的数字太小了！")
+    else:
+        print("恭喜您，猜对了，666")
+        break  # 跳出循环
+
+print("随机生成的数字是：", random_num)
+
+print("------------------------")
+
+# 练习：完成如下需求
+# 需求1：将1-1000之间（含1000）所有的5的倍数的数字累加起来。
+total = 0
+for i in range(1, 1001):
+    if i % 5 == 0:
+        total += i
+print(f"1-1000之间所有5的倍数的累加之和为：{total}")
+
+print("------------------------")
+
+# 需求2：统计字符串 "akiwksjakdiklowiqaamnvbamvaxnsjdsjkaaxkjd" 字符串中有多少个a和k。
+str1 = "akiwksjakdiklowiqaamnvbamvaxnsjdsjkaaxkjd"
+count_a = 0
+count_k = 0
+for char in str1:
+    if char == "a":
+        count_a += 1
+    elif char == "k":
+        count_k += 1
+print(f"字符串中a的个数为：{count_a}")
+print(f"字符串中k的个数为：{count_k}")
+
